21a3d861a4c11ad5412135fee2028408a1d49492
Jakub <devcobbier@gmail.com>
2022-08-15 16:47:57 +0200

    Added start screen component and setted up global stylesheets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StartScreenComponent } from './components/start-screen/start-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/start',
+    pathMatch: 'full',
+  },
+  { path: 'start', component: StartScreenComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
